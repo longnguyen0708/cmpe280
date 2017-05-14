fcf0e6e63d82f15606219e11843bf5c1010dccf3
nehaparmarcs <np6.023@gmail.com>
2017-05-14 14:21:47 -0700

    Updating missed file

---
+++ src/app/model/item.ts
@@ -0,0 +1,12 @@
+export class Item {
+  id: number;
+  name: string;
+  imgUrl: string;
+  category: string;
+  description: string;
+  d1: string;
+  d2: string;
+  d3: string;
+  price: string;
+  
+}
