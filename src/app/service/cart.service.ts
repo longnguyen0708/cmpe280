8473e9d090dd3a96a248e65611b313761cc2e30b
longnguyen0708 <long.t.nguyen@sjsu.edu>
2017-05-15 19:16:45 -0700

    fix add to cart same item multiple times

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -16,16 +16,33 @@ export class CartService {
 
   addToCart(uid: string, item: Item) {
     console.log('addToCart', item.price, parseFloat(item.price.substr(1)))
-    var cartItems = this.firebase.database.list(`/cart/${uid}`);
-    let cartItem = new CartItem();
-    cartItem.user_id =  uid;
-    cartItem.item_id = item.id;
-    cartItem.name = item.name;
-    cartItem.imgUrl = item.imgUrl;
-    cartItem.qty = 1;
-    cartItem.unit_price = parseFloat(item.price.substr(1));
+    let existingItem = this.firebase.database.list(`/cart/${uid}`, {
+      query: {
+        limitToFirst: 1,
+        orderByChild: 'item_id',
+        equalTo: item.id,
+      }
+    });
+    let objectSubscription = existingItem.subscribe(items => {
+      console.log('exisintItem', items);
+      objectSubscription.unsubscribe();
+      var cartItems = this.firebase.database.list(`/cart/${uid}`);
+      if (items.length == 0) {
+        let cartItem = new CartItem();
+        cartItem.user_id =  uid;
+        cartItem.item_id = item.id;
+        cartItem.name = item.name;
+        cartItem.imgUrl = item.imgUrl;
+        cartItem.qty = 1;
+        cartItem.unit_price = parseFloat(item.price.substr(1));
 
-    cartItems.push(cartItem);
+        cartItems.push(cartItem);
+      } else {
+        cartItems.update(items[0].$key, {qty: items[0].qty + 1})
+      }
+    })
+
+
   }
 
 }
